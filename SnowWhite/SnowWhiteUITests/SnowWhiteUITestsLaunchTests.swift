bc4c7b7d8830d8734774d9d9a6feccb1c9dddfcc
이윤채 <130755399+leeyoonchae@users.noreply.github.com>
2023-12-27 23:11:35 +0900

    create

---
+++ SnowWhite/SnowWhiteUITests/SnowWhiteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SnowWhiteUITestsLaunchTests.swift
+//  SnowWhiteUITests
+//
+//  Created by dgsw8th61 on 12/27/23.
+//
+
+import XCTest
+
+final class SnowWhiteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
